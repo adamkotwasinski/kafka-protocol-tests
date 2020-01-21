8d5de1d51a6971b0f7ffe17da6c6bb3e26433fd8
Adam Kotwasinski <adam.kotwasinski@gmail.com>
2020-01-21 14:23:14 -0800

    Init commit
    Signed-off-by: Adam Kotwasinski <adam.kotwasinski@gmail.com>

---
+++ src/test/java/tests/MockTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class MockTest {
+
+    @Test
+    public void test() {
+        // given
+        // when
+        // then
+        fail("fail");
+    }
+
+}
